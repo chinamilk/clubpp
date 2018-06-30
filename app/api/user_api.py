b3292f837638085ea92d99ca1f4498a398c80977
cuppar <cuppar.hzy@gmail.com>
2018-06-30 22:48:46 +0800

    完成usersApi

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -3,40 +3,78 @@
 
 """
 from flask_restful import Resource
+from flask import request
+import json
 
 class UsersApi(Resource):
-    def post(self, req):
-        '''对应 /api/users
+    def post(self):
+        '''对应 /api/users -post
 
         :param req: 添加的用户数据
         :return: 添加后的用户数据包括id
         '''
-        pass
-
-    def put(self, user_id, req):
+        req=request.json
+        return {'/api/users': req}
+    def put(self, user_id):
         '''
-        对应 /api/users/:user_id
+        对应 /api/users/:user_id -put
         :param user: 更新的用户数据
         :return: 更新对应user_id的用户数据
         '''
-        pass
+        req=request.form['foo']
+        return {'/api/users/:user_id -put': 'successful',
+                'user_id': user_id,
+                'req': req
+                }
+    def get(self):
+        '''
+        根据是否有username的query_string判断返回一个用户or所有用户
+        :return: 一个username相关用户or所有用户
+        '''
+        if request.query_string:
+            results={
+                'username': str(request.query_string, encoding='utf-8')
+            }
+        else:
+            results={
+                'all': '[users]'
+            }
+        return results
 
+class UsersApiGetById(Resource):
+    def get(self, user_id):
+        '''
+        /api/users/:user_id -get
+        根据id获取用户数据
+        :param user_id: 用户id
+        :return: 该id的用户数据
+        '''
+        return {
+            '/api/users/:user_id -get': 'successful',
+            'user_id': user_id
+        }
 
-
-
-# example for restful
-class HelloWorld(Resource):
-    """each method is corresponding to a request method(GET, POST, PUT, DELETE)
-
-    """
-    def get(self, rate):
-        return {'hello': rate}
-
-    def put(self):
-        return {'put': 'successful'}
-
-    def post(self):
-        return {'post': 'successful'}
-
-    def delete(self):
-        return {'delete': 'successful'}
+# class UsersApiGetByName(Resource):
+#     def get(self):
+#         '''
+#         /api/users?name=cuppar -get
+#         根据用户名获取用户数据
+#         :param username: 用户名
+#         :return: 该用户名的用户数据
+#         '''
+#         username=request.query_string.username
+#         return {
+#             '/api/users?name=cuppar -get': 'userMsg',
+#             'username': username
+#         }
+#
+# class UsersApiGetAll(Resource):
+#     def get(self):
+#         '''
+#         /api/users -get
+#         获取所有用户
+#         :return: 所有用户数据（列表）
+#         '''
+#         return {
+#             '/api/users -get': '[msgs]'
+#         }
--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from flask_restful import Api
 from app import create_app
 from app.api.club_api import ClubsApi
 from app.api.image_api import FileUploadApi
-from app.api.user_api import HelloWorld
+from app.api.user_api import UsersApi,UsersApiGetById
 
 app = create_app()
 api = Api(app)
 
-api.add_resource(HelloWorld, '/<int:rate>', '/hello/<int:rate>')
 api.add_resource(FileUploadApi, '/upload', endpoint="file_upload")
 
 api.add_resource(ClubsApi, '/api/clubs')
+
+# cuppar
+api.add_resource(UsersApi, '/api/users')
+api.add_resource(UsersApiGetById, '/api/users/<int:user_id>', '/api/users/<int:user_id>/')
+# api.add_resource(UsersApiGetByName, '/api/users?username=')
+# api.add_resource(UsersApiGetAll, '/api/users', '/api/users/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
