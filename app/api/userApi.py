49e7ae3c09bf936d1161603cd7fbf26a8909d790
cuppar <cuppar.hzy@gmail.com>
2018-06-30 19:59:14 +0800

    添加虚拟环境

--- app/api/userApi.py
+++ app/api/userApi.py
@@ -4,6 +4,25 @@
 """
 from flask_restful import Resource
 
+class UsersApi(Resource):
+    def post(self, req):
+        '''对应 /api/users
+
+        :param req: 添加的用户数据
+        :return: 添加后的用户数据包括id
+        '''
+        pass
+
+    def put(self, user_id, req):
+        '''
+        对应 /api/users/:user_id
+        :param user: 更新的用户数据
+        :return: 更新对应user_id的用户数据
+        '''
+        pass
+
+
+
 
 # example for restful
 class HelloWorld(Resource):
