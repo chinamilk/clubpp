760967b49165669f849c9264d7ecaf395d2cdfc3
cuppar <cuppar.hzy@gmail.com>
2018-07-05 00:22:21 +0800

    修改了user_dao.py中的更新函数

--- app/dao/user_dao.py
+++ app/dao/user_dao.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8-*-
 from app import db
 from app.dao import User
+from app import util
 
 
 def add_user(user: User):  # 测试通过
@@ -12,8 +13,18 @@ def add_user(user: User):  # 测试通过
 def update_user(user_id: 'str-要更新的用户id', user: User):  # 测试通过
     the_user = User.query.get(user_id)
     if isinstance(the_user, User):
-        db.session.delete(the_user)
-        db.session.add(user)
+        util.add_attribute(the_user, 'username', user.username)
+        util.add_attribute(the_user, 'name', user.name)
+        util.add_attribute(the_user, 'password', user.password)
+        util.add_attribute(the_user, 'email', user.email)
+        util.add_attribute(the_user, 'day_of_birth', user.day_of_birth)
+        util.add_attribute(the_user, 'gender', user.gender)
+        util.add_attribute(the_user, 'academy', user.academy)
+        util.add_attribute(the_user, 'major', user.major)
+        util.add_attribute(the_user, 'bio', user.bio)
+        util.add_attribute(the_user, 'phone', user.phone)
+        util.add_attribute(the_user, 'year_of_enrollment', user.year_of_enrollment)
+
         db.session.commit()
         result = User.query.get(user_id)
     else:
@@ -38,4 +49,3 @@ def get_users():  # 测试通过
 
 def get_user_by_id(user_id: int):  # 测试通过
     return User.query.filter_by(user_id=user_id).first()
-
